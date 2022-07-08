bd645ac83c4699a6c742c7f73f9fe7a1e2821237
Federico Juretich <fedejuret@gmail.com>
2022-07-07 21:05:03 -0300

    incluimos vistas

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
+const path = require('path');
 
 require('dotenv').config()
-
 const express = require('express');
+
 const app = express();
+
+app.get('/', function (solitud, respuesta) {
+
+    respuesta.sendFile(path.join(__dirname , 'views/index.html'));
+    
+});
+
+app.get('/contacto', function (solitud, respuesta) {
+
+    respuesta.sendFile(path.join(__dirname , 'views/contacto.html'));
+
+});
+
 
 app.listen(process.env.PUERTO, function () {
     console.log('Servidor iniciado en puerto: ' + process.env.PUERTO);
